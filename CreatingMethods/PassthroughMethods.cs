f8893905fb6129c44493a44bc3a02f2f309c2caf
dmitri_shepelev <shepelev.dm@gmail.com>
2020-10-19 16:19:02 +0300

    Оказывается нужно удалять комментарии, печалька.

--- CreatingMethods/PassthroughMethods.cs
+++ CreatingMethods/PassthroughMethods.cs
@@ -8,40 +8,28 @@
             return boolValue;
         }
 
-        // TODO #1: Add a static method here with name "ReturnValue" that gets "byteValue" parameter ("byte" type) and returns it.
         public static byte ReturnValue(byte byteValue) => byteValue;
 
-        // TODO #2: Add a static method here with name "ReturnValue" that gets "sbyteValue" parameter ("sbyte" type) and returns it.
         public static sbyte ReturnValue(sbyte sbyteValue) => sbyteValue;
 
-        // TODO #3: Add a static method here with name "ReturnValue" that gets "charValue" parameter ("char" type) and returns it.
         public static char ReturnValue(char charValue) => charValue;
 
-        // TODO #4: Add a static method here with name "ReturnValue" that gets "decimalValue" parameter ("decimal" type) and returns it.
         public static decimal ReturnValue(decimal decimalValue) => decimalValue;
 
-        // TODO #5: Add a static method here with name "ReturnValue" that gets "doubleValue" parameter ("double" type) and returns it.
         public static double ReturnValue(double doubleValue) => doubleValue;
 
-        // TODO #6: Add a static method here with name "ReturnValue" that gets "floatValue" parameter ("float" type) and returns it.
         public static float ReturnValue(float floatValue) => floatValue;
 
-        // TODO #7: Add a static method here with name "ReturnValue" that gets "intValue" parameter ("int" type) and returns it.
         public static int ReturnValue(int intValue) => intValue;
 
-        // TODO #8: Add a static method here with name "ReturnValue" that gets "uintValue" parameter ("uint" type) and returns it.
         public static uint ReturnValue(uint uintValue) => uintValue;
 
-        // TODO #9: Add a static method here with name "ReturnValue" that gets "longValue" parameter ("long" type) and returns it.
         public static long ReturnValue(long longValue) => longValue;
 
-        // TODO #10: Add a static method here with name "ReturnValue" that gets "ulongValue" parameter ("ulong" type) and returns it.
         public static ulong ReturnValue(ulong ulongValue) => ulongValue;
 
-        // TODO #11: Add a static method here with name "ReturnValue" that gets "shortValue" parameter ("short" type) and returns it.
         public static short ReturnValue(short shortValue) => shortValue;
 
-        // TODO #12: Add a static method here with name "ReturnValue" that gets "ushortValue" parameter ("ushort" type) and returns it.
         public static ushort ReturnValue(ushort ushortValue) => ushortValue;
     }
 }
